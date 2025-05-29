fa688677cadd6926a9ee67d8a83af6ddd58e2a35
Emma <emmcoy7@gmail.com>
2025-05-29 13:57:29 +0200

    feat: add service layer for User

--- src/main/java/com/example/rentalCar/service/UserService.java
+++ src/main/java/com/example/rentalCar/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.rentalCar.service;
 
+import com.example.rentalCar.model.User;
+
 public interface UserService {
+    void saveUser(User user);
+    User getUserById(String userId, String operation);
+    void deleteUser(String userId, String operation);
 }
+
--- src/main/java/com/example/rentalCar/service/UserServiceImpl.java
+++ src/main/java/com/example/rentalCar/service/UserServiceImpl.java
@@ -1,4 +1,30 @@
 package com.example.rentalCar.service;
 
-public class UserServiceImpl {
+import com.example.rentalCar.model.User;
+import com.example.rentalCar.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(String userId, String operation) {
+        return userRepository.findById(userId, operation);
+    }
+
+    @Override
+    public void deleteUser(String userId, String operation) {
+        userRepository.deleteById(userId, operation);
+    }
 }
+
