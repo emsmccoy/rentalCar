85ea2578bf9409320cd8fd0ffde665c9662a229d
Emma <emmcoy7@gmail.com>
2025-05-29 13:45:40 +0200

    feat: add service layer for Booking

--- src/main/java/com/example/rentalCar/service/BookingService.java
+++ src/main/java/com/example/rentalCar/service/BookingService.java
@@ -1,4 +1,11 @@
 package com.example.rentalCar.service;
 
+import com.example.rentalCar.model.Booking;
+import java.util.List;
+
 public interface BookingService {
+    void saveBooking(Booking booking);
+    Booking getBookingById(String userId, String operation);
+    List<Booking> getBookingsByUserId(String userId);
+    void deleteBooking(String userId, String operation);
 }
--- src/main/java/com/example/rentalCar/service/BookingServiceImpl.java
+++ src/main/java/com/example/rentalCar/service/BookingServiceImpl.java
@@ -1,4 +1,37 @@
 package com.example.rentalCar.service;
 
-public class BookingServiceImpl {
+import com.example.rentalCar.model.Booking;
+import com.example.rentalCar.repository.BookingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingServiceImpl implements BookingService {
+    private final BookingRepository bookingRepository;
+
+    public BookingServiceImpl(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    @Override
+    public void saveBooking(Booking booking) {
+        bookingRepository.save(booking);
+    }
+
+    @Override
+    public Booking getBookingById(String userId, String operation) {
+        return bookingRepository.findById(userId, operation);
+    }
+
+    @Override
+    public List<Booking> getBookingsByUserId(String userId) {
+        return bookingRepository.findByUserId(userId);
+    }
+
+    @Override
+    public void deleteBooking(String userId, String operation) {
+        bookingRepository.deleteById(userId, operation);
+    }
 }
+
